30836391878fb017ba873dfde22a0f7aa217238f
yella <yella@LAPTOP-Q9ML3J2S>
2022-04-15 10:06:28 +0530

    hhh

--- src/main/java/com/mvc/demo/DemoProject1Application.java
+++ src/main/java/com/mvc/demo/DemoProject1Application.java
@@ -8,7 +8,7 @@ public class DemoProject1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProject1Application.class, args);
-		System.out.println("hello");
+		System.out.println("hello got it change");
 	}
 
 }
